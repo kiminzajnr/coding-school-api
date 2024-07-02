4c57fefd0c3330a73c04bf36195cb4a2877b22bb
kiminzajnr <kiminzaeric@gmail.com>
2024-07-02 07:46:31 +0300

    Add route to create a new project

--- app.py
+++ app.py
@@ -25,3 +25,14 @@ def create_course():
     new_course = {"name": request_data["name"], "projects": []}
     courses.append(new_course)
     return new_course, 201
+
+@app.post("/course/<string:name>/project")
+def create_project(name):
+    request_data = request.get_json()
+    for course in courses:
+        if course["name"] == name:
+            new_project = {"name": request_data["name"], "description": request_data["description"]}
+            course["projects"].append(new_project)
+            return new_project
+        
+    return {"message": "Course not found"}, 404
