3ae7d8a47137c974efa93fc6973fdc12bf880cf8
kiminzajnr <kiminzaeric@gmail.com>
2024-07-03 22:56:40 +0300

    Add new endpoints

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 
+from flask_smorest import abort
 from flask import Flask, request
 
 from db import courses, projects
@@ -28,7 +29,7 @@ def create_project():
     project_data = request.get_json()
     if project_data["course_id"] not in courses:
         return {"message": "Course not found"}, 404
-    project_id = uuid.uuid4.hex
+    project_id = uuid.uuid4().hex
     project = {**project_data, "id": project_id}
     projects[project_id] = project
 
@@ -39,11 +40,39 @@ def get_course(course_id):
     try:
         return courses[course_id]
     except KeyError:
-        return {"message": "Course not found"}, 404
+        abort(404, message="Course not found.")
 
-@app.get("/course/<string:project_id>")
-def get_project_in_course(project_id):
+@app.get("/project/<string:project_id>")
+def get_project(project_id):
     try:
         return projects[project_id]
     except KeyError:
-        return {"message": "Project not found"}, 404
+        abort(404, message="Project not found.")
+
+@app.delete("/project/<string:project_id>")
+def delete_project(project_id):
+    try:
+        del projects[project_id]
+        return {"message": "Project deleted."}
+    except KeyError:
+        abort(404, message="Project not found.")
+
+@app.put("/project/<string:project_id>")
+def update_project(project_id):
+    project_data = request.get_json()
+    try:
+        project = projects[project_id]
+        project |= project_data
+
+        return project
+    
+    except KeyError:
+        abort(404, message="Project not found.")
+
+@app.delete("/course/<string:course_id>")
+def delete_course(course_id):
+    try:
+        del courses[course_id]
+        return {"message": "Course deleted."}
+    except KeyError:
+        abort(404, message="Course not found.")
