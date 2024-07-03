5c86983ba433ab5179ed63f03c082c2b77ee3f82
kiminzajnr <kiminzaeric@gmail.com>
2024-07-03 19:49:14 +0300

    Update: data model improvements

--- app.py
+++ app.py
@@ -1,52 +1,49 @@
+import uuid
+
 from flask import Flask, request
+
+from db import courses, projects
 
 app = Flask(__name__)
 
-courses = [
-    {
-        "name": "My Course 1", "projects": [
-            {
-                "name": "My Project 1", "description": "My favourite project"
-            },
-            {
-                "name": "My Project 1", "description": "My favourite project"
-            }
-        ]
-    }
-]
 
 @app.get("/course")
 def get_courses():
-    return {"courses": courses}
+    return {"courses": list(courses.values())}
+
+@app.get("/project")
+def get_projects():
+    return {"projects": list(projects.values())}
 
 @app.post("/course")
 def create_course():
-    request_data = request.get_json()
-    new_course = {"name": request_data["name"], "projects": []}
-    courses.append(new_course)
-    return new_course, 201
+    course_data = request.get_json()
+    course_id = uuid.uuid4().hex
+    course = {**course_data, "id": course_id}
+    courses[course_id] = course
+    return course, 201
 
-@app.post("/course/<string:name>/project")
-def create_project(name):
-    request_data = request.get_json()
-    for course in courses:
-        if course["name"] == name:
-            new_project = {"name": request_data["name"], "description": request_data["description"]}
-            course["projects"].append(new_project)
-            return new_project
-        
-    return {"message": "Course not found"}, 404
+@app.post("/project")
+def create_project():
+    project_data = request.get_json()
+    if project_data["course_id"] not in courses:
+        return {"message": "Course not found"}, 404
+    project_id = uuid.uuid4.hex
+    project = {**project_data, "id": project_id}
+    projects[project_id] = project
 
-@app.get("/course/<string:name>")
-def get_course(name):
-    for course in courses:
-        if course["name"] == name:
-            return course
-    return {"message": "Course not found"}, 404
+    return project
+    
+@app.get("/course/<string:course_id>")
+def get_course(course_id):
+    try:
+        return courses[course_id]
+    except KeyError:
+        return {"message": "Course not found"}, 404
 
-@app.get("/course/<string:name>/project")
-def get_project_in_course(name):
-    for course in courses:
-        if course["name"] == name:
-            return {"projects": course["projects"]}
-    return {"message": "Course not found"}, 404
+@app.get("/course/<string:project_id")
+def get_project_in_course(project_id):
+    try:
+        return projects[project_id]
+    except KeyError:
+        return {"message": "Project not found"}, 404
