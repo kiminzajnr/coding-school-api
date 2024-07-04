667f1b863291eb9a1ef154e6665d9cc09e764559
kiminzajnr <kiminzaeric@gmail.com>
2024-07-04 22:40:14 +0300

    Add one to many relationshipwith AQLAlchemy

--- models/courses.py
+++ models/courses.py
@@ -3,5 +3,8 @@ from db import db
 
 class CourseModel(db.Model):
     __tablename__ = "courses"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(1000), unique=True, nullable=False)
+
+    projects = db.relationship("ProjectModel", back_populates="course", lazy="dynamic")
