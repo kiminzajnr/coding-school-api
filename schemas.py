93de8efd5150c6361c53fdd7027588f576901df8
kiminzajnr <kiminzaeric@gmail.com>
2024-07-04 23:19:22 +0300

    Update marshmallow schemas

--- schemas.py
+++ schemas.py
@@ -1,16 +1,22 @@
 from marshmallow import Schema, fields
 
 
-class ProjectSchema(Schema):
+class PlainProjectSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     description = fields.Str(required=True)
-    course_id = fields.Str(required=True)
+
+class PlainCourseSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
+class ProjectSchema(PlainProjectSchema):
+    course_id = fields.Int(required=True, load_only=True)
+    course = fields.Nested(PlainCourseSchema(), dump_only=True)
 
 class ProjectUpdateSchema(Schema):
     name = fields.Str()
     description = fields.Str()
 
-class CourseSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+class CourseSchema(PlainCourseSchema):
+    projects = fields.List(fields.Nested(PlainProjectSchema()), dump_only=True)
