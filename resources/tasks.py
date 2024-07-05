9e4704f3bdc6257033f4cd4b3552d0f6cbfcab69
kiminzajnr <kiminzaeric@gmail.com>
2024-07-05 16:02:39 +0300

    Add methods for tasks

---
+++ resources/tasks.py
@@ -0,0 +1,62 @@
+from flask_smorest import Blueprint, abort
+
+from flask.views import MethodView
+
+from sqlalchemy.exc import SQLAlchemyError
+
+from schemas import TaskSchema, TaskUpdateSchema
+
+from models import TaskModel
+
+from db import db
+
+
+blp = Blueprint("Tasks", __name__, description="Operations on tasks")
+
+
+@blp.route("/task/<int:task_id>")
+class Task(MethodView):
+    @blp.response(200, TaskSchema)
+    def get(self, task_id):
+        task = TaskModel.query.get_or_404(task_id)
+
+        return task
+    
+    def delete(self, task_id):
+        task = TaskModel.query.get_or_404(task_id)
+        db.session.delete(task)
+        db.session.commit()
+
+        return {"message": "Task deleted successfully."}, 200
+    
+    @blp.arguments(TaskUpdateSchema)
+    @blp.response(200, TaskSchema)
+    def put(self, task_data, task_id):
+        task = TaskModel.query.get(task_id)
+        if task:
+            task.title = task_data["title"]
+            task.description = task_data["description"]
+
+        else:
+            task = TaskModel(id=task_id, **task_data)
+
+        db.session.add(task)
+        db.session.commit()
+
+        return task
+
+@blp.route("/task")
+class TaskList(MethodView):
+    @blp.response(200, TaskSchema(many=True))
+    def get(self):
+        return TaskModel.query.all()
+    
+    @blp.arguments(TaskSchema)
+    @blp.response(201, TaskSchema)
+    def post(self, task_data):
+        task = TaskModel(**task_data)
+        try:
+            db.session.add(task)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the task")
