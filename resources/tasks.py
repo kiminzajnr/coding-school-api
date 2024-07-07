3a8f9a0db83d1f67a8dcdd2f9a5f17b96828ec9d
kiminzajnr <kiminzaeric@gmail.com>
2024-07-07 04:58:36 +0300

    Add protect resources with jwt required

--- resources/tasks.py
+++ resources/tasks.py
@@ -1,5 +1,7 @@
 from flask_smorest import Blueprint, abort
 
+from flask_jwt_extended import jwt_required
+
 from flask.views import MethodView
 
 from sqlalchemy.exc import SQLAlchemyError
@@ -22,6 +24,7 @@ class Task(MethodView):
 
         return task
     
+    @jwt_required()
     def delete(self, task_id):
         task = TaskModel.query.get_or_404(task_id)
         db.session.delete(task)
@@ -29,6 +32,7 @@ class Task(MethodView):
 
         return {"message": "Task deleted successfully."}, 200
     
+    @jwt_required()
     @blp.arguments(TaskUpdateSchema)
     @blp.response(200, TaskSchema)
     def put(self, task_data, task_id):
@@ -51,6 +55,7 @@ class TaskList(MethodView):
     def get(self):
         return TaskModel.query.all()
     
+    @jwt_required()
     @blp.arguments(TaskSchema)
     @blp.response(201, TaskSchema)
     def post(self, task_data):
