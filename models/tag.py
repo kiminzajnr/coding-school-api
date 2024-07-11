48440b7915aba68c4c0c0ec70d65a7e832226551
kiminzajnr <kiminzaeric@gmail.com>
2024-07-12 00:19:31 +0300

    Create SQLAlchemy model

--- models/tag.py
+++ models/tag.py
@@ -5,7 +5,7 @@ class TagModel(db.Model):
     __tablename__ = "tags"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unque=False, nullable=False)
-    store_id = db.Column(db.Integer, db.ForeignKey("courses.id", nullable=False))
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.id"), nullable=False)
 
     course = db.relationship("CourseModel", back_populates="tags")
