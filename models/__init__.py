8673affce59e6c8386bf1d8db72cfda2c9692037
kiminzajnr <kiminzaeric@gmail.com>
2024-07-14 00:55:05 +0300

    Import ProjectsTags

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from models.project import ProjectModel
 from models.task import TaskModel
 from models.user import UserModel
 from models.tag import TagModel
+from models.project_tags import ProjectsTags
