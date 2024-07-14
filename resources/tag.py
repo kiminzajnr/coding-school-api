78169394c26e529e4176bb5f16fb028451f3c0d8
kiminzajnr <kiminzaeric@gmail.com>
2024-07-14 15:52:09 +0300

    Add api endpoints for tags

--- resources/tag.py
+++ resources/tag.py
@@ -2,8 +2,8 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 
-from schemas import TagSchema
-from models import TagModel, CourseModel
+from schemas import TagSchema, TagAndProjectSchema
+from models import TagModel, CourseModel, ProjectModel
 from db import db
 
 
@@ -45,3 +45,59 @@ class Tag(MethodView):
         tag = TagModel.query.get_or_404(tag_id)
 
         return tag
+    
+    @blp.response(
+        202,
+        description="Deletes a tag if no project is tagged with it.",
+        example={"message": "Tag deleted."},
+    )
+    @blp.alt_response(404, description="Tag not found.")
+    @blp.alt_response(
+        400,
+        description="Returned if the tag is assigned to one or more projects. In this case the tag is not deleted.",
+    )
+    def delete(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+
+        if not tag.projects:
+            db.session.delete(tag)
+            db.session.commit()
+            return {"message": "Tag deleted."}
+        abort(
+            400,
+            message="Could not delete tag. Make sure the tag is not associated with any projects then try again.",
+        )
+    
+
+@blp.route("/project/<int:project_id>/tag/<int:tag_id>")
+class LinkTagsToProject(MethodView):
+    @blp.response(201, TagSchema)
+    def post(self, project_id, tag_id):
+        project = ProjectModel.query.get_or_404(project_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        project.tags.append(tag)
+
+        try:
+            db.session.add(project)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the tag.")
+
+        return tag
+    
+    @blp.response(200, TagAndProjectSchema)
+    def delete(self, project_id, tag_id):
+        project = ProjectModel.query.get_or_404(project_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        project.tags.remove(tag)
+
+        try:
+            db.session.add(project)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the tag.")
+        
+        return {"message": "Project removed from tag", "project": project, "tag": tag}
+    
