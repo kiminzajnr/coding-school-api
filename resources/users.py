8bd6334f3733bb3995fc0da0725d76bb0686b81e
kiminzajnr <kiminzaeric@gmail.com>
2024-07-24 21:34:38 +0300

    Add [feat] Send emails

--- resources/users.py
+++ resources/users.py
@@ -2,6 +2,8 @@ from flask.views import MethodView
 
 from flask_smorest import Blueprint, abort
 
+from sqlalchemy import or_
+
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
@@ -16,6 +18,7 @@ from schemas import UserSchema, UserRegisterSchema
 from models import UserModel
 from db import db
 from blocklist import BLOCKLIST
+from tasks import send_user_registration_email
 
 
 blp = Blueprint("Users", "users", description="Operations on users.")
@@ -23,17 +26,25 @@ blp = Blueprint("Users", "users", description="Operations on users.")
 
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"]
+            )
+        ).first():
             abort(409, message="A user with that username already exists.")
 
         user = UserModel(
             username=user_data["username"],
+            email=user_data["email"],
             password=pbkdf2_sha256.hash(user_data["password"])
         )
         db.session.add(user)
         db.session.commit()
+
+        send_user_registration_email(user.email, user.username)
 
         return {"message": "User created successfully."}, 201
     
